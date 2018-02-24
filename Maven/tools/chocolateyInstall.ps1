e651dfa15b95bac28499b7f59a2d404522a8fd27
Anthony Accioly <a.accioly@7rtc.com>
2018-02-24 15:45:28 +0000

    Remove unnecessary environment variables

--- Maven/tools/chocolateyInstall.ps1
+++ Maven/tools/chocolateyInstall.ps1
@@ -10,7 +10,6 @@ $name = "apache-maven-$version"
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
 $m2_home = Join-Path $package $name
-$m2_bin = Join-Path $m2_home 'bin'
 $mvn_cmd = Join-Path $m2_home 'bin/mvn.cmd'
 $mvn_debug_cmd = Join-Path $m2_home 'bin/mvnDebug.cmd'
 $m2_repo = Join-Path $env:USERPROFILE '.m2'
@@ -19,9 +18,6 @@ $url = "https://archive.apache.org/dist/maven/maven-3/$version/binaries/$name-bi
 
 
 [Environment]::SetEnvironmentVariable('M2_HOME', $m2_home, "User")
-[Environment]::SetEnvironmentVariable('MAVEN_OPTS', '-Xms256m', "User")
-[Environment]::SetEnvironmentVariable('M2', $m2_bin, "User")
-[Environment]::SetEnvironmentVariable('M2_REPO', $m2_repo, "User")
 
 Install-ChocolateyZipPackage `
     -PackageName 'Maven' `
